a45721e0d1d6ab6f625b771b513315b692e73736
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-08-03 01:16:34 +0300

    error handle for 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
