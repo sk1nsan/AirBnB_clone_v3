465f27b4cb6f17573b1820780a8225a63b5046e7
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-08-04 00:15:25 +0300

    store password in md5

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,10 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, attr, value):
+        if attr == 'password':
+            super().__setattr__(attr,
+                                hashlib.md5(str.encode(value)).hexdigest())
+        else:
+            super().__setattr__(attr, value)
