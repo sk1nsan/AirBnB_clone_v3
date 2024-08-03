6024232c0d68c85772971c07e796e3ce72013c8d
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-08-03 20:10:29 +0300

    created a new view for User objects that handles all default RESTFul API actions

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -48,6 +48,10 @@ def create_user():
         abort(400, 'Not a JSON')
     if ('name' not in request.get_json()):
         abort(400, 'Missing name')
+    if ('email' not in request.get_json()):
+        abort(400, 'Missing email')
+    if ('password' not in request.get_json()):
+        abort(400, 'Missing password')
     obj = User(**request.get_json())
     storage.new(obj)
     storage.save()
